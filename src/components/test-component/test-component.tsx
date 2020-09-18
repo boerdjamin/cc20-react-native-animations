3b6093eca050a3230d5d282957e7f60e87a1e9d0
Florian Stoehr <florian.stoehr@cap3.de>
2020-09-18 16:40:42 +0200

    Clean up animation

--- src/components/test-component/test-component.tsx
+++ src/components/test-component/test-component.tsx
@@ -1,5 +1,11 @@
 import * as React from 'react'
-import { Button, SafeAreaView, StyleSheet, View } from 'react-native'
+import {
+  Button,
+  Dimensions,
+  SafeAreaView,
+  StyleSheet,
+  View,
+} from 'react-native'
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
@@ -7,12 +13,15 @@ import Animated, {
 } from 'react-native-reanimated'
 import { CAP3_GREEN } from '../../styles/colors'
 
+const BOX_DIMENSIONS = 100
+
 const TestComponent = () => {
   const offset = useSharedValue(0)
+  const maxTranslateX = Dimensions.get('window').width - BOX_DIMENSIONS
 
   const animatedStyles = useAnimatedStyle(() => {
     return {
-      transform: [{ translateX: offset.value * 255 }],
+      transform: [{ translateX: offset.value * maxTranslateX }],
     }
   })
 
@@ -38,8 +47,10 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   box: {
-    width: 100,
-    height: 100,
+    width: BOX_DIMENSIONS,
+    height: BOX_DIMENSIONS,
     backgroundColor: CAP3_GREEN,
+    borderRadius: 16,
+    marginVertical: 32,
   },
 })
