6bed616bb77dfc02b12202fe551957bc66088786
Florian Stoehr <florian.stoehr@cap3.de>
2020-09-18 14:54:04 +0200

    Add animated component

--- src/components/test-component/test-component.tsx
+++ src/components/test-component/test-component.tsx
@@ -1,16 +1,42 @@
 import * as React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Button, SafeAreaView, StyleSheet, View } from 'react-native'
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated'
+import { CAP3_GREEN } from '../../styles/colors'
 
 const TestComponent = () => {
+  const offset = useSharedValue(0)
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: offset.value * 255 }],
+    }
+  })
+
   return (
-    <View style={styles.container}>
-      <Text>TestComponent</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={{ flex: 1 }}>
+        <Animated.View style={[styles.box, animatedStyles]} />
+        <Button
+          onPress={() => (offset.value = Math.random())}
+          title={'Press me'}
+        />
+      </View>
+    </SafeAreaView>
   )
 }
 
 export default TestComponent
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+  },
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: CAP3_GREEN,
+  },
 })
